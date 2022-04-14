9d45eb4d97d51f03f82741261308424f9d0ce13f
Valery Klachkov <zgdump@gmail.com>
2022-04-14 12:34:49 +1000

    Add #[must_use] and #[inline] attributes for BitPosition

--- src/bit_pos.rs
+++ src/bit_pos.rs
@@ -15,14 +15,18 @@ impl BitPosition {
         BitPosition { byte, bit }
     }
 
+    #[inline]
     pub fn bits(&self) -> usize {
         self.byte * 8 + self.bit
     }
 
+    #[inline]
     pub fn is_round(&self) -> bool {
         self.bit == 0
     }
 
+    #[inline]
+    #[must_use]
     pub fn round_up(&self) -> Self {
         BitPosition::new(
             if self.bit != 0 {
@@ -34,23 +38,33 @@ impl BitPosition {
         )
     }
 
+    #[inline]
+    #[must_use]
     pub fn round_down(&self) -> Self {
         BitPosition::new(self.byte, 0)
     }
 
+    #[inline]
+    #[must_use]
     pub fn inc_bytes(&self, bytes: usize) -> Self {
         BitPosition::new(self.byte + bytes, self.bit)
     }
 
+    #[inline]
+    #[must_use]
     pub fn inc_bits(&self, bits: usize) -> Self {
         let bits = self.bit + bits;
         BitPosition::new(self.byte + (bits / 8), bits % 8)
     }
 
+    #[inline]
+    #[must_use]
     pub fn sub_bytes(&self, bytes: usize) -> Self {
         BitPosition::new(self.byte - bytes, self.bit)
     }
 
+    #[inline]
+    #[must_use]
     pub fn sub_bits(&self, bits: usize) -> Self {
         let result_bits = self.bits() - bits;
         BitPosition::new(result_bits / 8, result_bits % 8)
